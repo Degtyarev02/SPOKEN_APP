16b5cf1616a21eca6dd45663ff7bb4c0ce4628fc
Vladimir <vladimirdegty@gmail.com>
2021-12-02 19:27:51 +0300

    best practice s3wrapper

--- src/main/java/com/example/twitterclone/service/S3Wrapper.java
+++ src/main/java/com/example/twitterclone/service/S3Wrapper.java
@@ -1,63 +1,44 @@
 package com.example.twitterclone.service;
 
-import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.regions.Region;
-import com.amazonaws.regions.Regions;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.model.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.apache.commons.io.IOUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Service;
-import org.springframework.util.StringUtils;
-import org.springframework.web.multipart.MultipartFile;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.URLEncoder;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 @Service
 public class S3Wrapper {
 
-	public AmazonS3Client amazonS3Client() {
-		AmazonS3Client amazonS3Client = new AmazonS3Client(new BasicAWSCredentials("AKIAYWPKIV2DI6AYK7UY", "I0m59qS3D5O+svBqZYJeUKYxuH1D501PtwyD4dPY"));
-		amazonS3Client.setRegion(Region.getRegion(Regions.US_EAST_2));
-		return amazonS3Client;
+
+	private final AmazonS3Client s3Client;
+	private final String bucket;
+
+	//создаем амазонклиент с помощью бина из StorageConfig конструктором
+	//Присваиваем названию bucket значение из application.properties
+	@Autowired
+	public S3Wrapper(AmazonS3Client s3Client, @Value("${aws.bucket.name}") String bucket) {
+		this.bucket = bucket;
+		this.s3Client = s3Client;
 	}
 
-	private final AmazonS3Client s3Client = amazonS3Client();
-
-	private String bucket = "spokenresourcesbucket";
-
+	//Метод загрузки фоток на aws s3
 	public PutObjectResult upload(InputStream inputStream, String uploadKey) {
+		//Получаем запрос на добавление, передаем в параметры имя bucket'а, имя файла, поток файла
 		PutObjectRequest putObjectRequest = new PutObjectRequest(bucket, uploadKey, inputStream, new ObjectMetadata());
-
 		putObjectRequest.setCannedAcl(CannedAccessControlList.PublicRead);
-
 		PutObjectResult putObjectResult = s3Client.putObject(putObjectRequest);
-
 		IOUtils.closeQuietly(inputStream);
-
 		return putObjectResult;
 	}
 
+	//Метод удаления файлов из aws s3
+	//Используется при удалении постов, где есть фотографии и при смене пользователем аватарки
 	public void deleteFile(final String keyName) {
 		final DeleteObjectRequest deleteObjectRequest = new DeleteObjectRequest(bucket, keyName);
 		s3Client.deleteObject(deleteObjectRequest);
 	}
-
-
 
 }
