d1ab568593ad24264cd9b14e671772ddfeab4964
Vladimir <vladimirdegty@gmail.com>
2021-12-05 20:18:42 +0300

    display comments to profile page

--- src/main/java/com/example/twitterclone/domain/Message.java
+++ src/main/java/com/example/twitterclone/domain/Message.java
@@ -15,113 +15,122 @@ import org.hibernate.annotations.CascadeType;
 @Entity
 public class Message {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.AUTO)
-	private Long id;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
-	@NotBlank(message = "Please fill the message")
-	@Length(max = 512, message = "Message is long then 512ch.")
-	private String text;
+    @NotBlank(message = "Please fill the message")
+    @Length(max = 512, message = "Message is long then 512ch.")
+    private String text;
 
-	@NotBlank(message = "Please fill the tag")
-	private String tag;
+    @NotBlank(message = "Please fill the tag")
+    private String tag;
 
-	@ManyToOne(fetch = FetchType.EAGER)
-	@JoinColumn(name = "user_id")
-	private User author;
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    private User author;
 
-	@OneToMany(fetch = FetchType.EAGER, orphanRemoval = true, cascade = javax.persistence.CascadeType.REMOVE)
-	private List<Comment> comments;
+    @OneToMany(fetch = FetchType.EAGER, orphanRemoval = true, cascade = javax.persistence.CascadeType.REMOVE)
+    private List<Comment> comments;
 
-	private Calendar date;
+    private Calendar date;
 
-	private String filename;
+    private String filename;
 
-	public Message(String text, String tag, User author) {
-		this.text = text;
-		this.author = author;
-		this.tag = tag;
-	}
+    public Message(String text, String tag, User author) {
+        this.text = text;
+        this.author = author;
+        this.tag = tag;
+    }
 
-	public Message() {
-	}
+    public Message() {
+    }
 
-	public Long getId() {
-		return id;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public String getText() {
-		return text;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	public String getTag() {
-		return tag;
-	}
+    public String getTag() {
+        return tag;
+    }
 
-	public void setTag(String tag) {
-		this.tag = tag;
-	}
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
 
-	public User getAuthor() {
-		return author;
-	}
+    public User getAuthor() {
+        return author;
+    }
 
-	public String getFilename() {
-		return filename;
-	}
+    public String getFilename() {
+        return filename;
+    }
 
-	public void setFilename(String filename) {
-		this.filename = filename;
-	}
-
-
-	public void setAuthor(User author) {
-		this.author = author;
-	}
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
 
 
-	public Calendar getDate() {
-		return date;
-	}
-
-	public List<Comment> getComments() {
-		return comments;
-	}
-
-	public void setComments(List<Comment> comments) {
-		this.comments = comments;
-	}
-
-	public String returnReformatDate() {
-		StringBuilder builder = new StringBuilder();
-
-		builder
-				.append(date.get(Calendar.HOUR_OF_DAY))
-				.append(":")
-				.append(date.get(Calendar.MINUTE));
-		return builder.toString();
-
-	}
-
-	public void setDate(Calendar date) {
-		this.date = date;
-	}
+    public void setAuthor(User author) {
+        this.author = author;
+    }
 
 
-	@Override
-	public String toString() {
-		return "Message{" +
-				"id=" + id +
-				", text='" + text + '\'' +
-				", tag='" + tag + '\'' +
-				'}';
-	}
+    public Calendar getDate() {
+        return date;
+    }
+
+    public List<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(List<Comment> comments) {
+        this.comments = comments;
+    }
+
+    public String returnReformatDate() {
+        StringBuilder builder = new StringBuilder();
+
+        //Если часов меньше 10, добавляем 0 в начале для двузначаного числа
+        if (date.get(Calendar.HOUR_OF_DAY) < 10) {
+            builder.append("0");
+        }
+
+        builder.append(date.get(Calendar.HOUR_OF_DAY)).append(":");
+
+        //Тоже самое что и с часами
+        if (date.get(Calendar.MINUTE) < 10) {
+            builder.append("0");
+        }
+
+        builder.append(date.get(Calendar.MINUTE));
+        return builder.toString();
+
+    }
+
+    public void setDate(Calendar date) {
+        this.date = date;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "id=" + id +
+                ", text='" + text + '\'' +
+                ", tag='" + tag + '\'' +
+                '}';
+    }
 }
