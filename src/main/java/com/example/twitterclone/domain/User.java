b6929f1fe1bc2631934596e12b9c13374322e720
Vladimir <vladimirdegty@gmail.com>
2021-12-10 16:10:52 +0300

    bugfix

--- src/main/java/com/example/twitterclone/domain/User.java
+++ src/main/java/com/example/twitterclone/domain/User.java
@@ -8,6 +8,7 @@ import javax.persistence.*;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import java.util.Collection;
+import java.util.List;
 import java.util.Set;
 
 @Entity
