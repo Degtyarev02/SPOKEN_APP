9ce62da823faf8571324777c590203ea8c6f4a35
Vladimir <vladimirdegty@gmail.com>
2022-01-10 14:02:01 +0300

    main upd to chat
    add fronted part

--- src/main/resources/static/js/ws.js
+++ src/main/resources/static/js/ws.js
@@ -1,15 +1,16 @@
 let stompClient = null;
 let selectedUser = document.getElementById('receiverId').value;
 let senderUser = document.getElementById('senderId').value;
+let noMessages = document.getElementById('no-messages');
 
 function connect() {
     const socket = new SockJS('/gs-guide-websocket');
     stompClient = Stomp.over(socket);
     stompClient.connect({}, function (frame) {
         console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/'+senderUser, function (response) {
+        stompClient.subscribe('/topic/' + senderUser, function (response) {
             let data = JSON.parse(response.body);
-            if(selectedUser === data.from){
+            if (selectedUser === data.from) {
                 showGreeting(data.message)
             }
         });
@@ -18,19 +19,28 @@ function connect() {
 
 
 document.getElementById('chat_send_button').onclick = function sendName() {
-    console.log("blyyyaa")
+
+    if (noMessages) {
+        noMessages.style.display = 'none';
+    }
+
     let message = document.getElementById('chat_text').value;
     let container = document.getElementById('all_chat_messages')
     let innerDiv = document.createElement('div');
     let text = document.createTextNode(message);
     document.getElementById('chat_text').value = '';
     innerDiv.appendChild(text);
+    innerDiv.classList.add("sender-message-block");
+    if (senderUser === selectedUser) {
+        innerDiv.classList.add("receiver-message");
+    }
     container.appendChild(innerDiv);
+
     sendMessage(senderUser, selectedUser, message);
 }
 
-function sendMessage(from, to, text){
-    stompClient.send("/app/"+selectedUser, {}, JSON.stringify({
+function sendMessage(from, to, text) {
+    stompClient.send("/app/" + selectedUser, {}, JSON.stringify({
         from: from,
         to: to,
         message: text
@@ -41,6 +51,10 @@ function showGreeting(chatMessage) {
     let container = document.getElementById('all_chat_messages')
     let innerDiv = document.createElement('div');
     let text = document.createTextNode(chatMessage);
+    innerDiv.classList.add("sender-message-block");
+    if (senderUser !== selectedUser) {
+        innerDiv.classList.add("receiver-message");
+    }
     innerDiv.appendChild(text);
     container.appendChild(innerDiv);
 }
